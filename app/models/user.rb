998a9e474c9b59b7b46737238d2600f558e136d7
JEAN-BAPTISTE <jgeorges24@icloud.com>
2021-07-12 18:44:00 -0400

    coding the relationship associatnions as well as validaitons for user object

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ApplicationRecord
 
+has_secure_password
 
+has_many :tapes
+has_many :created_mixtapes, foreign_key: "user_id", class_name: "Mixtape"
 
+#user validations
+validates :username, presence: true, uniqueness: true
+validates :password, presence: true, length: { in: 5..30}
+validates :name, presence: true
 
 
 
