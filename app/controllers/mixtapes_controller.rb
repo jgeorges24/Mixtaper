97633944ff88b2d9dffab6101d9970554a6b02e6
JEAN-BAPTISTE <jgeorges24@icloud.com>
2021-07-12 17:22:31 -0400

    cofnigured the views for index.html for mixtape model

--- app/controllers/mixtapes_controller.rb
+++ app/controllers/mixtapes_controller.rb
@@ -2,6 +2,20 @@ class MixtapesController < ApplicationController
 
 
 
+    def index
+        @mixtapes = Mixtape.all
+    end
+
+    def show
+        @mixtape = Mixtape.find_by(id: params [:id])
+    end
+
+    def new 
+
+        @mixtape = Mixtape.new
+
+    end
+
 
 
 end
