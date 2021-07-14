8e3f4cf7cd82e5878e3182bb6da188532f9a6259
JEAN-BAPTISTE <jgeorges24@icloud.com>
2021-07-14 00:39:50 -0400

    mixtape_controller setting up the crud for this modle

--- app/controllers/mixtapes_controller.rb
+++ app/controllers/mixtapes_controller.rb
@@ -1,19 +1,57 @@
 class MixtapesController < ApplicationController
-  def edit
-  end
+    
+    def index
+        #@mixtapes = Mixtape.includes(:user)
+        @mixtapes = Mixtape.all
+    end
+    
+    def show
+        @mixtape = Mixtape.find_by(id: params[:id])
+    end
 
-  def index
-  end
 
-  def new
-  end
+    def new
 
-  def recent_mixtape
-  end
+        @mixtape = Mixtape.new
+        
+    end
 
-  def search
-  end
+    def create
+        @mixtape = Mixtape.new(mixtape_params)
+        
+        # @mixtape = current_user.created_tapes.build(tape_params)
+        
+        if @tape.save
+            redirect_to tape_path(@tape)
+        else
+            flash[:message] = @tape.errors.full_messages.to_sentence 
+            redirect_to new_tape_path
+    
+    
+    end
+    
+    def edit
+        @mixtape = Mixtape.find_by(id: params[:id])
 
-  def show
-  end
+    end
+  
+    def recent_mixtape
+        @mixtapes = Mixtape.recent_mixtape
+
+    end
+
+  
+    def search
+        @mixtapes = Mixtape.search(params[:title])
+    end
+
+
+    private
+    
+    def mixtape_params(*args)
+        # params.require(:tape).permit(:title, :artist, :features, :artcover, :genre)
+        (*args)
+    end
+
+
 end
